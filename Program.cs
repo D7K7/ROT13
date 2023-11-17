408f1ae159bbe0ef7ce7750a290b9c882e733d60
D7K7 <dakhjohnson@proton.me>
2023-11-16 18:26:38 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Kata
+{
+  public static string Rot13(string input)
+  {
+    Span<char> translation = stackalloc char[input.Length];
+    input.AsSpan().CopyTo(translation);
+    
+    for (int i = 0; i < translation.Length; i++)
+    {
+        char currentChar = translation[i];
+      
+        if (char.IsLetter(currentChar))
+        {
+            char baseChar = char.IsUpper(currentChar) ? 'A' : 'a';
+            translation[i] = (char)((((currentChar - baseChar) + 13) % 26) + baseChar);
+        }
+    }
+    
+    return translation.ToString();
+  }
+}
